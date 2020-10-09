6517381bf36568212e1282d3ee2ace1b2c55b186
Andrew Riachi <brasswood@users.noreply.github.com>
2020-10-08 19:41:40 -0500

    add a 5 second delay to getting/printing max temp

--- Telemetry_Transmitter/Telemetry_Transmitter.ino
+++ Telemetry_Transmitter/Telemetry_Transmitter.ino
@@ -1,7 +1,13 @@
 #include "DueCANLayer.h"
+const byte BYTE_MIN = -128;
+const unsigned long DELAY = 5000;
 
+byte maxTemp;
+unsigned long nextTimeWeSendPacket;
 void setup()
 {
+  maxTemp = -128;
+  nextTimeWeSendPacket = 0;
   // Set the serial interface baud rate
   Serial.begin(115200);
     // Initialize both CAN controllers
@@ -23,10 +29,22 @@ void loop()
     if(canRx(0, &lMsgID, &bExtendedFormat, &cRxData[0], &cDataLen) == CAN_OK)
     {
       if (lMsgID == 0x6B1) {
-        Serial.print("High Temperature: ");
-        Serial.print(cRxData[4]);
-        Serial.print("\n\r");
+        if (cRxData[4] > maxTemp)
+          maxTemp = cRxData[4];
       }
     } // end if
   }
+  if (millis() >= nextTimeWeSendPacket)
+  {
+    nextTimeWeSendPacket += DELAY;
+    printTemperature(maxTemp);
+    maxTemp = BYTE_MIN;
+  }
+}
+
+void printTemperature(byte temp)
+{
+  Serial.print("High Temperature: ");
+  Serial.print(temp);
+  Serial.print("\n\r");
 }
