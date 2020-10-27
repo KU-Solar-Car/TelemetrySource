5b7c7fbac79d285e41ea370de9fb1669e4b8ad36
Andrew Riachi <brasswood@users.noreply.github.com>
2020-10-26 23:49:53 -0500

    initial XBee library
    I wrote a lot of code and I have no idea if it works! But here are some
    things to note:
    
    First, I wrote some public helper methods (such as sendAtCommand())
    which take Strings by reference and then concatenate them into new
    Strings. This may lead to heap fragmentation, I really don't know. A
    probably safer way to implement this is to have a legitimate transmit
    buffer.
    
    Secondly, I wrote a read() method. It returns a userPacket, which is a
    struct that contains references to either null values (NULL_USER_PACKET)
    or references to parts of a receive buffer (m_rxBuffer). There are some
    things that I don't know will work properly:
    1. When I set m_rxBuffer = {} to clear the buffer, will the contained
    String frameData be deallocated? If so, will its reallocation and
    re-reservation in the constructor cause heap fragmentation?
    2. What actually happens when I return NULL_USER_PACKET, since I'm not
    returning the struct itself by reference, but the struct *contains*
    references?
    
    Anyway, I'm pretty proud of this. Even if it doesn't work. At least it
    compiles.

---
+++ Telemetry_Transmitter/XBee.cpp
@@ -0,0 +1,106 @@
+/* File: XBee.cpp
+ * Author: Andrew Riachi
+ * Date: 2020-10-15
+ * Description: Implementation of the XBee class
+ */
+#include "XBee.h"
+#include <Arduino.h>
+
+packet::packet()
+{
+  // Helps prevent heap fragmentation (even though it's very possible in the rest of my code :/)
+  frameData.reserve(1550);
+}
+
+
+void XBee::sendFrame(byte frameType, const String& frameData)
+{
+  uint16_t checksum = 0;
+  uint16_t len = frameData.length();
+  for (uint16_t i = 0; i < len; i++)
+    checksum += frameData[i];
+  checksum = 0xFF - checksum;
+
+  const byte start = 0xFE;
+  byte len_msb = (byte) len >> 8;
+  byte len_lsb = (byte) len;
+
+  m_serial.write(start);
+  m_serial.write(len_msb);
+  m_serial.write(len_lsb);
+  m_serial.write(frameType);
+  m_serial.write(frameData.c_str());
+  m_serial.write(checksum);
+}
+
+void XBee::sendATCommand(const String& command, const String& param)
+{
+  sendFrame(0x08, command+param);
+}
+
+void XBee::shutDown()
+{
+  sendATCommand("SD", "0");
+}
+
+userPacket XBee::read()
+{
+  // To save memory, I would like userPacket to contain references to m_rxBuffer.
+  // Therefore, once we get a non-empty userPacket, we need it to be valid at least until the next call
+  // to read().
+  // So here, we'll check if our last call to read() received a full packet and if so, clear m_rxBuffer
+  // to make room for a brand new packet. (This means userPacket will no longer have your previous packet!)
+  if (m_rxBuffer.recvd_checksum)
+  {
+    m_rxBuffer = {};
+  }
+  // find the first field that hasn't been received yet.
+  // if we don't have the full packet yet, return ""
+  for (int recvd = m_serial.read(); (recvd != -1 && !m_rxBuffer.recvd_checksum); recvd = m_serial.read())
+  {
+    if (!m_rxBuffer.recvd_start)
+    {
+      m_rxBuffer.recvd_start = (recvd == 0x7E);
+    }
+    else if (!m_rxBuffer.recvd_len_msb)
+    {
+      m_rxBuffer.recvd_len_msb = true;
+      m_rxBuffer.length = recvd << 8;
+    }
+    else if (!m_rxBuffer.recvd_len_lsb)
+    {
+      m_rxBuffer.recvd_len_lsb = true;
+      m_rxBuffer.length += recvd;
+    }
+    else if (!m_rxBuffer.recvd_frameType)
+    {
+      m_rxBuffer.recvd_frameType = true;
+      m_rxBuffer.frameType = recvd;
+    }
+    else if (m_rxBuffer.frameData.length() != m_rxBuffer.length-1) // -1 to account for the frameType
+    {
+      m_rxBuffer.frameData += (char) recvd; // use a char here because that is probably what append is
+      // defined for
+    }
+    else
+    {
+      m_rxBuffer.recvd_checksum = true;
+      m_rxBuffer.checksum = recvd;
+    }
+  }
+  // if we are done looping because we received the checksum
+  if (m_rxBuffer.recvd_checksum)
+  {
+    // calculate the checksum
+    uint8_t my_checksum = m_rxBuffer.frameType; // frameType is technically the start of frameData
+    for (unsigned int i = 0; i < m_rxBuffer.length; i++)
+    {
+      my_checksum += (uint8_t) m_rxBuffer.frameData.charAt(i);
+    }
+    if (m_rxBuffer.recvd_checksum == my_checksum)
+      return userPacket{m_rxBuffer.frameType, m_rxBuffer.frameData};
+    else // poo poo packet, clear the buffer and return nothing to our poor user :(
+      m_rxBuffer = {};
+  }
+  return NULL_USER_PACKET;
+}
---
+++ Telemetry_Transmitter/XBee.h
@@ -0,0 +1,53 @@
+/* File: XBee.h
+ * Author: Andrew Riachi
+ * Date: 2020-10-15
+ * Description: Functions to help send packets using the Digi module
+ */
+#ifndef XBEE_H
+#define XBEE_H
+
+#include <Arduino.h>
+#include <string>
+
+struct packet
+{
+  packet();
+  bool recvd_start;
+  bool recvd_len_msb;
+  bool recvd_len_lsb;
+  bool recvd_frameType;
+  bool recvd_checksum;
+  uint16_t length;
+  uint8_t frameType;
+  String frameData;
+  uint8_t checksum;
+};
+
+struct userPacket
+{
+  const uint8_t& frameType;
+  const String& frameData;
+};
+
+const userPacket NULL_USER_PACKET = {0, ""};
+
+class XBee
+{
+  private:
+  HardwareSerial& m_serial;
+  packet m_rxBuffer;
+
+  public:
+  XBee(HardwareSerial &serial) : m_serial(serial) {}
+  void configure(const String& server);
+  void openConnection();
+  void closeConnection();
+  void sendPacket(const String& message);
+  userPacket read(); // Can read one full packet at a time.
+  bool waitFor(const String& message, int timeout);
+  void shutDown();
+  void sendFrame(byte frameType, const String& frameData);
+  void sendATCommand(const String& command, const String& param);
+};
+
+#endif
