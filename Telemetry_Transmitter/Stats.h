e946ba90a2e327de46bff6fec8f98925e26208fe
Andrew Riachi <brasswood@users.noreply.github.com>
2021-02-18 20:23:50 -0600

    turn StatData into a tagged union
    Turn StatData into a tagged union and make a method that converts it to
    a key-value pair for use in the json.

--- Telemetry_Transmitter/Stats.h
+++ Telemetry_Transmitter/Stats.h
@@ -1,31 +1,22 @@
 #ifndef STATS_H
 #define STATS_H
 
-union StatData
+struct StatData
 {
-  double doubleVal;
-  unsigned uIntVal;
-  bool boolVal;
+  enum {BATT_VOLTAGE, BATT_CURRENT, BATT_TEMP, BMS_FAULT, GPS_TIME, GPS_LAT, GPS_LON, GPS_VEL_EAST, GPS_VEL_NOR, GPS_VEL_UP, GPS_SPD, SOLAR_VOLTAGE, SOLAR_CURRENT, MOTOR_SPD, _LAST} name;
+  union {
+    double doubleVal;
+    unsigned uIntVal;
+    bool boolVal;
+  };
 };
 
-enum StatTypes
-{
-  BATT_VOLTAGE, BATT_CURRENT, BATT_TEMP, BMS_FAULT, GPS_TIME, GPS_LAT, GPS_LON, GPS_VEL_EAST, GPS_VEL_NOR, GPS_VEL_UP, GPS_SPD, SOLAR_VOLTAGE, SOLAR_CURRENT, MOTOR_SPD
-};
-
-struct Optional
+struct OptionalData
 {
   bool present;
   StatData value;
 };
 
-class Stats
-{
-  //Optional batteryVoltage, batteryCurrent, batteryTemperature, bmsFault, gpsTime, gpsLat, gpsLon, gpsVelocityEast, gpsVelocityNorth, gpsVelocityUp, gpsSpeed, solarVoltage, solarCurrent, motorSpeed;
-  Optional m_carData[14];
-
-  // Method for setting for each data type in the union
-  // Method for getting string to send through XBee  
-};
+typedef OptionalData Stats[StatData::_LAST];
 
 #endif
--- Telemetry_Transmitter/Telemetry_Transmitter.ino
+++ Telemetry_Transmitter/Telemetry_Transmitter.ino
@@ -33,7 +33,6 @@ void setup()
   else
     Serial.print("CAN0: Initialization Failed.\n\r");
 
-  strcpy(requestBuffer, "POST /car HTTP/1.1\r\nHost: ku-solar-car-b87af.appspot.com\r\nContent-Type: application/json\r\nAuthentication: eiw932FekWERiajEFIAjej94302Fajde\r\n\r\n");
 
 }
 
@@ -41,7 +40,7 @@ void loop()
 {
   // sendMaxTempEveryFiveSeconds();
   printReceivedFrame();
-  sendStats();
+  // sendStats();
   shutdownOnCommand();
 }
 
@@ -98,10 +97,37 @@ void printTemperature(byte temp)
 
 void sendStats(Stats stats)
 {
-  unsigned positionInString = 0;
+  strcpy(requestBuffer, "POST /car HTTP/1.1\r\nHost: ku-solar-car-b87af.appspot.com\r\nContent-Type: application/json\r\nAuthentication: eiw932FekWERiajEFIAjej94302Fajde\r\n\r\n");
   strcat(requestBuffer, "{");
-  if 
-  \"battery_voltage\":%06f,\"battery_current\":%06f,\"battery_temperature\":%06f,\"bms_fault\":%d,\"gps_time\":%06f,\"gps_lat\":%06f,\"gps_lon\":%06f,\"gps_velocity_east\":%06f,\"gps_velocity_north\":%06f,\"gps_velocity_up\":%06f,\"gps_speed\":%06f,\"solar_voltage\":%06f,\"solar_current\":%06f,\"motor_speed\":%06f}";
-  request
+  for (int i = 0; i < StatData::_LAST; i++)
+  {
+    if (stats[i].present)
+    {
+      toKeyValuePair(requestBuffer + strlen(requestBuffer), stats[i].value); // append the key-value pair
+      strcat(requestBuffer, ",");
+    }
+    strcat(requestBuffer, "}");
+  }
   
+}
+
+void toKeyValuePair(char* dest, StatData data)
+{
+  switch(data.name)
+  {
+    case StatData::BATT_VOLTAGE: sprintf(dest, "\"battery_voltage\":%06f", data.doubleVal); break;
+    case StatData::BATT_CURRENT: sprintf(dest, "\"battery_current\":%06f", data.doubleVal); break;
+    case StatData::BATT_TEMP: sprintf(dest, "\"battery_temperature\":%06f", data.doubleVal); break;
+    case StatData::BMS_FAULT: sprintf(dest, "\"bms_fault\":%d", data.boolVal); break;
+    case StatData::GPS_TIME: sprintf(dest, "\"gps_time\":%06f", data.uIntVal); break;
+    case StatData::GPS_LAT: sprintf(dest, "\"gps_lat\":%06f", data.doubleVal); break;
+    case StatData::GPS_LON: sprintf(dest, "\"gps_lon\":%06f", data.doubleVal); break;
+    case StatData::GPS_VEL_EAST: sprintf(dest, "\"gps_velocity_east\":%06f", data.doubleVal); break;
+    case StatData::GPS_VEL_NOR: sprintf(dest, "\"gps_velocity_north\":%06f", data.doubleVal); break;
+    case StatData::GPS_VEL_UP: sprintf(dest, "\"gps_velocity_up\":%06f", data.doubleVal); break;
+    case StatData::GPS_SPD: sprintf(dest, "\"gps_speed\":%06f", data.doubleVal); break;
+    case StatData::SOLAR_VOLTAGE: sprintf(dest, "\"solar_voltage\":%06f", data.doubleVal); break;
+    case StatData::SOLAR_CURRENT: sprintf(dest, "\"solar_current\":%06f", data.doubleVal); break;
+    case StatData::MOTOR_SPD: sprintf(dest, "\"motor_speed\":%06f", data.doubleVal); break;
+  }
 }
