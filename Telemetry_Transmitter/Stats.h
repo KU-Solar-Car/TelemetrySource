eb28baa94adf9a06d17b61f984490837efa48546
Andrew Riachi <brasswood@users.noreply.github.com>
2021-03-13 14:43:01 -0600

    Make StatData and Stats simpler

--- Telemetry_Transmitter/Stats.h
+++ Telemetry_Transmitter/Stats.h
@@ -1,22 +1,36 @@
 #ifndef STATS_H
 #define STATS_H
 
+enum StatKey: int
+{
+  BATT_VOLTAGE,
+  BATT_CURRENT,
+  BATT_TEMP,
+  BMS_FAULT,
+  GPS_TIME,
+  GPS_LAT,
+  GPS_LON,
+  GPS_VEL_EAST,
+  GPS_VEL_NOR,
+  GPS_VEL_UP,
+  GPS_SPD,
+  SOLAR_VOLTAGE,
+  SOLAR_CURRENT,
+  MOTOR_SPD,
+  _LAST
+};
+
 struct StatData
 {
-  enum {BATT_VOLTAGE, BATT_CURRENT, BATT_TEMP, BMS_FAULT, GPS_TIME, GPS_LAT, GPS_LON, GPS_VEL_EAST, GPS_VEL_NOR, GPS_VEL_UP, GPS_SPD, SOLAR_VOLTAGE, SOLAR_CURRENT, MOTOR_SPD, _LAST} name;
   union {
     double doubleVal;
     unsigned uIntVal;
     bool boolVal;
   };
-};
-
-struct OptionalData
-{
   bool present;
-  StatData value;
+
 };
 
-typedef OptionalData Stats[StatData::_LAST];
+typedef StatData Stats[StatKey::_LAST];
 
 #endif
--- Telemetry_Transmitter/Telemetry_Transmitter.ino
+++ Telemetry_Transmitter/Telemetry_Transmitter.ino
@@ -6,8 +6,9 @@
 #include "Frames.h"
 
 const byte BYTE_MIN = -128;
-const unsigned long DELAY = 5000;
 byte maxTemp;
+
+const unsigned long DELAY = 5000;
 unsigned long nextTimeWeSendFrame;
 MonitoredSerial mySerial(Serial1, Serial);
 XBee xbee(mySerial);
@@ -19,66 +20,65 @@ Stats testStats;
 
 void setup()
 {
+  // Set the serial interface baud rate
   Serial.begin(9600);
   Serial1.begin(9600);
-  
-  delay(5000);
+
+  /* --------------------------------
+   * Set the XBee in API Mode
+   * =================================*/
   if(xbee.configure())
     Serial.println("Configuration successful");
   else
     Serial.println("Configuration failed");
-  maxTemp = -128;
-  nextTimeWeSendFrame = 0;
-  // Set the serial interface baud rate
-    // Initialize both CAN controllers
-  Serial.println("Test");  
+    
+  /* =================================
+   * Initialize CAN board
+   * =================================*/
   if(canInit(0, CAN_BPS_250K) == CAN_OK)
     Serial.print("CAN0: Initialized Successfully.\n\r");
   else
     Serial.print("CAN0: Initialization Failed.\n\r");
-
-  StatData tmp;
-  tmp.name = StatData::BATT_VOLTAGE;
-  tmp.doubleVal = 420.69;
-  //test_stats[0] = {true, {StatData::BATT_VOLTAGE, .doubleVal=69.420}};
-  testStats[0].present = true;
-  testStats[0].value = tmp;
-  for (int i = 1; i < StatData::_LAST; i++)
+  
+  /* =================================
+   * Initialize StatData
+   * =================================*/
+  testStats[StatKey::BATT_VOLTAGE].present = true;
+  testStats[StatKey::BATT_VOLTAGE].doubleVal = 420.69;
+  for (int k = 1; k < StatKey::_LAST; k++)
   {
     //test_stats[i] = {false, {i, .boolVal=false}};
-    testStats[i].present = false;
+    testStats[k].present = false;
   }
+
+  /* =================================
+   * Wait for modem to associate before starting 
+   * =================================*/
+  userFrame status;
+  Serial.println("Waiting for network to associate...");
+  do
+  {
+    status = xbee.read();
+  } while(!(status.frameType == 0x8A && status.frameData[0] == 2));
+  Serial.println("Network associated.");
+  
+  /* =================================
+   * Initialize variables that track stuff
+   * =================================*/
+  maxTemp = -128;
+  nextTimeWeSendFrame = 0;
 }
 
 void loop()
 {
   // sendMaxTempEveryFiveSeconds();
+  
   printReceivedFrame();
   sendStatsEveryFiveSeconds(testStats);
   shutdownOnCommand();
 }
 
-void sendMaxTempEveryFiveSeconds()
-{
-  // Check for received message
-  long lMsgID;
-  bool bExtendedFormat;
-  byte cRxData[8];
-  byte cDataLen;
-  if(canRx(0, &lMsgID, &bExtendedFormat, &cRxData[0], &cDataLen) == CAN_OK)
-  {
-    if (lMsgID == 0x6B1) {
-      if (cRxData[4] > maxTemp)
-        maxTemp = cRxData[4];
-    }
-  } // end if
-  if (millis() >= nextTimeWeSendFrame)
-  {
-    nextTimeWeSendFrame += DELAY;
-    printTemperature(maxTemp);
-    maxTemp = BYTE_MIN;
-  }
-}
+
 
 void printReceivedFrame()
 {
@@ -112,12 +112,6 @@ void shutdownOnCommand()
   }
 }
 
-void printTemperature(byte temp)
-{
-  Serial.print("High Temperature: ");
-  Serial.print(temp);
-  Serial.print("\n\r");
-}
 
 void setContentLengthHeader(char* dest, int len)
 {
@@ -145,12 +139,12 @@ void sendStats(Stats stats)
   strcpy(requestBuffer, "POST /car HTTP/1.1\r\nContent-Length: 000\r\nHost: ku-solar-car-b87af.appspot.com\r\nContent-Type: application/json\r\nAuthentication: eiw932FekWERiajEFIAjej94302Fajde\r\n\r\n");
   strcat(requestBuffer, "{");
   int bodyLength = 2;
-  for (int i = 0; i < StatData::_LAST; i++)
+  for (int k = 0; k < StatKey::_LAST; k++)
   {
-    if (stats[i].present)
+    if (stats[k].present)
     {
-      bodyLength += toKeyValuePair(requestBuffer + strlen(requestBuffer), stats[i].value); // append the key-value pair
-      //strcat(requestBuffer, ",");
+      bodyLength += toKeyValuePair(requestBuffer + strlen(requestBuffer), k, stats[k]); // append the key-value pair
+      strcat(requestBuffer, ",");
     }
   }
   
@@ -164,23 +158,56 @@ void sendStats(Stats stats)
   // xbee.sendTCP(IPAddress(54, 166, 163, 67), 443, 0, 0, requestBuffer, strlen(requestBuffer));
 }
 
-int toKeyValuePair(char* dest, StatData data)
+int toKeyValuePair(char* dest, int key, StatData data)
 {
-  switch(data.name)
+  switch(key)
   {
-    case StatData::BATT_VOLTAGE: return sprintf(dest, "\"battery_voltage\":%6f", data.doubleVal); break;
-    case StatData::BATT_CURRENT: return sprintf(dest, "\"battery_current\":%6f", data.doubleVal); break;
-    case StatData::BATT_TEMP: return sprintf(dest, "\"battery_temperature\":%6f", data.doubleVal); break;
-    case StatData::BMS_FAULT: return sprintf(dest, "\"bms_fault\":%d", data.boolVal); break;
-    case StatData::GPS_TIME: return sprintf(dest, "\"gps_time\":%6f", data.uIntVal); break;
-    case StatData::GPS_LAT: return sprintf(dest, "\"gps_lat\":%6f", data.doubleVal); break;
-    case StatData::GPS_LON: return sprintf(dest, "\"gps_lon\":%6f", data.doubleVal); break;
-    case StatData::GPS_VEL_EAST: return sprintf(dest, "\"gps_velocity_east\":%6f", data.doubleVal); break;
-    case StatData::GPS_VEL_NOR: return sprintf(dest, "\"gps_velocity_north\":%6f", data.doubleVal); break;
-    case StatData::GPS_VEL_UP: return sprintf(dest, "\"gps_velocity_up\":%6f", data.doubleVal); break;
-    case StatData::GPS_SPD: return sprintf(dest, "\"gps_speed\":%6f", data.doubleVal); break;
-    case StatData::SOLAR_VOLTAGE: return sprintf(dest, "\"solar_voltage\":%6f", data.doubleVal); break;
-    case StatData::SOLAR_CURRENT: return sprintf(dest, "\"solar_current\":%6f", data.doubleVal); break;
-    case StatData::MOTOR_SPD: return sprintf(dest, "\"motor_speed\":%6f", data.doubleVal); break;
+    case StatKey::BATT_VOLTAGE: return sprintf(dest, "\"battery_voltage\":%6f", data.doubleVal); break;
+    case StatKey::BATT_CURRENT: return sprintf(dest, "\"battery_current\":%6f", data.doubleVal); break;
+    case StatKey::BATT_TEMP: return sprintf(dest, "\"battery_temperature\":%6f", data.doubleVal); break;
+    case StatKey::BMS_FAULT: return sprintf(dest, "\"bms_fault\":%d", data.boolVal); break;
+    case StatKey::GPS_TIME: return sprintf(dest, "\"gps_time\":%6f", data.uIntVal); break;
+    case StatKey::GPS_LAT: return sprintf(dest, "\"gps_lat\":%6f", data.doubleVal); break;
+    case StatKey::GPS_LON: return sprintf(dest, "\"gps_lon\":%6f", data.doubleVal); break;
+    case StatKey::GPS_VEL_EAST: return sprintf(dest, "\"gps_velocity_east\":%6f", data.doubleVal); break;
+    case StatKey::GPS_VEL_NOR: return sprintf(dest, "\"gps_velocity_north\":%6f", data.doubleVal); break;
+    case StatKey::GPS_VEL_UP: return sprintf(dest, "\"gps_velocity_up\":%6f", data.doubleVal); break;
+    case StatKey::GPS_SPD: return sprintf(dest, "\"gps_speed\":%6f", data.doubleVal); break;
+    case StatKey::SOLAR_VOLTAGE: return sprintf(dest, "\"solar_voltage\":%6f", data.doubleVal); break;
+    case StatKey::SOLAR_CURRENT: return sprintf(dest, "\"solar_current\":%6f", data.doubleVal); break;
+    case StatKey::MOTOR_SPD: return sprintf(dest, "\"motor_speed\":%6f", data.doubleVal); break;
   }
 }
+
+
+/* =================================
+ * Temporarily not being used
+ * =================================*/
+//void sendMaxTempEveryFiveSeconds()
+//{
+//  // Check for received message
+//  long lMsgID;
+//  bool bExtendedFormat;
+//  byte cRxData[8];
+//  byte cDataLen;
+//  if(canRx(0, &lMsgID, &bExtendedFormat, &cRxData[0], &cDataLen) == CAN_OK)
+//  {
+//    if (lMsgID == 0x6B1) {
+//      if (cRxData[4] > maxTemp)
+//        maxTemp = cRxData[4];
+//    }
+//  } // end if
+//  if (millis() >= nextTimeWeSendFrame)
+//  {
+//    nextTimeWeSendFrame += DELAY;
+//    printTemperature(maxTemp);
+//    maxTemp = BYTE_MIN;
+//  }
+//}
+
+//void printTemperature(byte temp)
+//{
+//  Serial.print("High Temperature: ");
+//  Serial.print(temp);
+//  Serial.print("\n\r");
+//}
